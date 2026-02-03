4b391af16b76803d32f67357e25ffc8480127e75
Abhay Thorat <abhaythorat79@gmail.com>
2026-02-03 09:54:40 +0800

    Dsa day2

---
+++ src/ReverseArray.java
@@ -0,0 +1,22 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] a = {10, 20, 30, 40, 50};
+
+        int start = 0;
+        int end = a.length - 1;
+
+        while (start < end) {
+            int temp = a[start];
+            a[start] = a[end];
+            a[end] = temp;
+
+            start++;
+            end--;
+        }
+        // Print reversed array
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
