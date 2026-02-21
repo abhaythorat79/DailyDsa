f34d8b510aeebab41e51c2abf5d7068e4d718e46
Abhay Thorat <abhaythorat79@gmail.com>
2026-02-21 20:38:08 +0800

    Dsa question

---
+++ src/String/ReverseString.java
@@ -0,0 +1,26 @@
+package String;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "hello";
+
+        char[] arr = str.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        String reversed = new String(arr);
+        System.out.println("Reversed: " + reversed);
+    }
+}
