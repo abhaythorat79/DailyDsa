5fad17adbf0df28880e26eb8c61d1f714290121f
Abhay Thorat <abhaythorat79@gmail.com>
2026-02-23 21:00:16 +0800

    Dsa question

---
+++ src/String/CharacterFrequency.java
@@ -0,0 +1,21 @@
+package String;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+
+        String str = "banana";
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char)i + " → " + freq[i]);
+            }
+        }
+    }
+}
