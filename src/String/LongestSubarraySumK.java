b8e0f99e3b89fa1be516a3227cc756f44adf8ee0
Abhay Thorat <abhaythorat79@gmail.com>
2026-03-05 19:29:20 +0800

    Dsa question

---
+++ src/String/LongestSubarraySumK.java
@@ -0,0 +1,27 @@
+package String;
+
+public class LongestSubarraySumK {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 2, 1, 7, 8, 1, 2, 8};
+        int k = 8;
+
+        int left = 0;
+        int sum = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+
+            sum += arr[right];
+
+            while (sum > k) {
+                sum -= arr[left];
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        System.out.println("Longest length: " + maxLength);
+    }
+}
