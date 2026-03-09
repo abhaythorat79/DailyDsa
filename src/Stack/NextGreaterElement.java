02d579ea5a8954eb97a01532ecd4d692a5d7c727
Abhay Thorat <abhaythorat79@gmail.com>
2026-03-09 21:32:26 +0800

    Dsa question

---
+++ src/Stack/NextGreaterElement.java
@@ -0,0 +1,24 @@
+package Stack;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 2, 25};
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            while (!stack.isEmpty() && arr[i] > stack.peek()) {
+                System.out.println(stack.pop() + " → " + arr[i]);
+            }
+
+            stack.push(arr[i]);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + " → -1");
+        }
+    }
+}
