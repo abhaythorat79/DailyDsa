4e9f81a936313f94bf989ebe5c3d4586a4ca8e9d
Abhay Thorat <abhaythorat79@gmail.com>
2026-08-22 11:51:08 +0530

    das

--- src/tas/Demo.java
+++ src/tas/Demo.java
@@ -1,4 +1,23 @@
 package tas;
 
 public class Demo {
+
+    public static void main(String[] args){
+       int arr[]={1,2,3,4,5,6,7};
+       // 5,6,7,1,2,3,4,
+        int possi=2;
+
+        int n=arr.length;
+
+        possi=possi%n;
+
+        int result[] =new int[n];
+
+       for(int i=0;i<n;i++){
+           result[(i+possi) %n]=arr[i];
+       }
+       for(int num : result){
+           System.out.print(num+ " ");
+       }
+    }
 }
